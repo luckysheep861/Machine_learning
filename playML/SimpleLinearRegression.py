04150184fb522766f2a93a1a106a1a70467b9ee6
luckysheep861 <707258043@qq.com>
2018-05-25 11:01:25 +0800

    5-4 向量化

--- playML/SimpleLinearRegression.py
+++ playML/SimpleLinearRegression.py
@@ -20,14 +20,14 @@ class SimpleLinearRegression1:
             num += (x - x_mean) * (y - y_mean)
             d += (x - x_mean) ** 2
 
-        self.a = num / d
-        self.b = y_mean - self.a_ * x_mean
+        self.a_ = num / d
+        self.b_ = y_mean - self.a_ * x_mean
 
         return self
 
     def predict(self, x_predict):
         #x_predict,是一个一位数组
-        assert x_predict.ndim = 1, \
+        assert x_predict.ndim == 1, \
                "this alg can only solve single feature training data."
         assert self.a_ is not None and self.b_ is not None, \
                "must fit before predict"
@@ -39,3 +39,40 @@ class SimpleLinearRegression1:
 
     def __repr__(self):
         return "SimpleLinearRegresson1()"
+
+class SimpleLinearRegression2: #向量化之后的简单线性回归
+    def __init__(self):
+        self.a_ = None
+        self.b_ = None
+
+    def fit(self, x_train, y_train):
+        assert x_train.ndim ==1, "x_train.ndim must equal to 1"
+        assert len(x_train) == len(y_train), \
+               "the size of x_train must equal to the size of y_train"
+
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+
+        num = (x_train - x_mean).dot(y_train - y_mean)
+        d = (x_train - x_mean).dot(x_train - x_mean)
+        
+
+        self.a_ = num / d
+        self.b_ = y_mean - self.a_ * x_mean
+
+        return self
+
+    def predict(self, x_predict):
+        #x_predict,是一个一位数组
+        assert x_predict.ndim == 1, \
+               "this alg can only solve single feature training data."
+        assert self.a_ is not None and self.b_ is not None, \
+               "must fit before predict"
+
+        return np.array([self._predict(x) for x in x_predict])
+
+    def _predict(self, x_single):
+        return self.a_ * x_single + self.b_
+
+    def __repr__(self):
+        return "SimpleLinearRegresson2()"
